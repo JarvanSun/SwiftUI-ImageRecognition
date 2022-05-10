7102d3b603ea14a4d6b87c922185e951f0ec44da
JarvanSun <614577202@qq.com>
2022-05-10 16:12:42 +0800

    Create project

---
+++ SwiftUI-ImageRecognition/SwiftUI-ImageRecognition/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-ImageRecognition
+//
+//  Created by JarvanSun on 2022/5/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-ImageRecognition/SwiftUI-ImageRecognition/SwiftUI_ImageRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ImageRecognitionApp.swift
+//  SwiftUI-ImageRecognition
+//
+//  Created by JarvanSun on 2022/5/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ImageRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
